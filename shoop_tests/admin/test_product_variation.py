tekmickey0118/Shuup

commit 43a88d2092a525ac1785588443d2789c095403c5

Fix simple variation form (and add tests)
Refs SHOOP-1075

shoop_tests/admin/test_product_variation.py
@@ -1,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from django.forms import formset_factory
+import pytest
+from shoop.admin.modules.products.views.variation.simple_variation_forms import SimpleVariationChildForm, SimpleVariationChildFormSet
+from shoop.testing.factories import create_product
+from shoop_tests.utils import printable_gibberish
+from shoop_tests.utils.forms import get_form_data
+
+
+@pytest.mark.django_db
+def test_simple_children_formset():
+    FormSet = formset_factory(SimpleVariationChildForm, SimpleVariationChildFormSet, extra=5, can_delete=True)
+    parent = create_product(printable_gibberish())
+    child = create_product(printable_gibberish())
+
+    # No links yet
+    formset = FormSet(parent_product=parent)
+    assert formset.initial_form_count() == 0  # No children yet
+
+    # Save a link
+    data = dict(get_form_data(formset, True), **{"form-0-child": child.pk})
+    formset = FormSet(parent_product=parent, data=data)
+    formset.save()
+    assert parent.variation_children.filter(pk=child.pk).exists()  # Got link'd!
+
+    # Remove the link
+    formset = FormSet(parent_product=parent)
+    assert formset.initial_form_count() == 1  # Got the child here
+    data = dict(get_form_data(formset, True), **{"form-0-DELETE": "1"})
+    formset = FormSet(parent_product=parent, data=data)
+    formset.save()
+    assert not parent.variation_children.exists()  # Got unlinked

shoop/admin/modules/products/views/variation/edit_variation.py
@@ -41,7 +41,7 @@ class VariationChildrenFormPart(FormPart):
             form,
             template_name=template_name,
             required=False,
-            kwargs={"parent_product": product}
+            kwargs={"parent_product": product, "request": self.request}
         )
 
     def form_valid(self, form):

shoop/admin/modules/products/views/variation/simple_variation_forms.py
@@ -21,7 +21,8 @@ class SimpleVariationChildForm(forms.Form):
 class SimpleVariationChildFormSet(BaseFormSet):
     def __init__(self, **kwargs):
         kwargs.pop("empty_permitted", None)
-        self.parent_product = kwargs.pop("parent_product", None)
+        self.request = kwargs.pop("request", None)
+        self.parent_product = kwargs.pop("parent_product")
         kwargs["initial"] = [
             {"child": product}
             for product
@@ -63,5 +64,5 @@ class SimpleVariationChildFormSet(BaseFormSet):
             message_parts.append(_("New: %d") % len(products_to_add))
         if products_to_remove:
             message_parts.append(_("Removed: %d") % len(products_to_remove))
-        if message_parts:
+        if message_parts and self.request:
             messages.success(self.request, ", ".join(message_parts))

shoop_tests/utils/forms.py
@@ -5,7 +5,9 @@
 #
 # This source code is licensed under the AGPLv3 license found in the
 # LICENSE file in the root directory of this source tree.
+from itertools import chain
 from django.core.exceptions import ValidationError
+from django.forms import BaseFormSet
 
 
 def get_form_data(form, prepared=False):
@@ -14,7 +16,7 @@ def get_form_data(form, prepared=False):
     be directly passed back as `data` to a form to simulate form submissions.
 
     :param form: A form.
-    :type form: django.forms.Form
+    :type form: django.forms.BaseForm|django.forms.BaseFormSet
     :param prepared: Prepare the values?
     :type prepared: bool
     :return: Dict of data
@@ -22,6 +24,12 @@ def get_form_data(form, prepared=False):
     """
 
     # This is based on Django's `django.forms.forms.BaseForm::changed_data` method.
+
+    if isinstance(form, BaseFormSet):
+        data = {}
+        for subform in chain([form.management_form], form.forms):
+            data.update(get_form_data(subform, prepared=prepared))
+        return data
 
     data = {}
     for name, field in form.fields.items():

commit 540bd413d7766ad023fc1b1413e120033e8d9051

Fix variable variation form regression
Regression introduced in 43a88d2092a525ac1785588443d2789c095403c5

Refs SHOOP-1075

shoop/admin/modules/products/views/variation/variable_variation_forms.py
@@ -24,7 +24,8 @@ from shoop.utils.multilanguage_model_form import to_language_codes
 
 class VariableVariationChildrenForm(forms.Form):
     def __init__(self, **kwargs):
-        self.parent_product = kwargs.pop("parent_product", None)
+        self.parent_product = kwargs.pop("parent_product")
+        self.request = kwargs.pop("request", None)
         super(VariableVariationChildrenForm, self).__init__(**kwargs)
         self._build_fields()
 

shoop/core/models/product_variation.py
@@ -128,13 +128,13 @@ def get_combination_hash_from_variable_mapping(parent, variables):
     """
     mapping = {}
     for variable_identifier, value_identifier in variables.items():
-        variable_identifier, _ = ProductVariationVariable.objects.get_or_create(
-            product=parent, identifier=variable_identifier
+        variable, _ = ProductVariationVariable.objects.get_or_create(
+            product=parent, identifier=force_text(variable_identifier)
         )
-        value_identifier, _ = ProductVariationVariableValue.objects.get_or_create(
-            variable=variable_identifier, identifier=value_identifier
+        value, _ = ProductVariationVariableValue.objects.get_or_create(
+            variable=variable, identifier=force_text(value_identifier)
         )
-        mapping[variable_identifier] = value_identifier
+        mapping[variable] = value
     return hash_combination(mapping)
 
 

shoop_tests/admin/test_product_variation.py
@@ -2,6 +2,8 @@
 from django.forms import formset_factory
 import pytest
 from shoop.admin.modules.products.views.variation.simple_variation_forms import SimpleVariationChildForm, SimpleVariationChildFormSet
+from shoop.admin.modules.products.views.variation.variable_variation_forms import VariableVariationChildrenForm
+from shoop.core.models.product_variation import ProductVariationVariable, ProductVariationVariableValue
 from shoop.testing.factories import create_product
 from shoop_tests.utils import printable_gibberish
 from shoop_tests.utils.forms import get_form_data
@@ -30,3 +32,19 @@ def test_simple_children_formset():
     formset = FormSet(parent_product=parent, data=data)
     formset.save()
     assert not parent.variation_children.exists()  # Got unlinked
+
+
+@pytest.mark.django_db
+def test_variable_variation_form():
+    var1 = printable_gibberish()
+    var2 = printable_gibberish()
+    parent = create_product(printable_gibberish())
+    for a in range(4):
+        for b in range(3):
+            child = create_product(printable_gibberish())
+            child.link_to_parent(parent, variables={var1: a, var2: b})
+    assert parent.variation_children.count() == 4 * 3
+
+    form = VariableVariationChildrenForm(parent_product=parent)
+    assert len(form.fields) == 12
+    # TODO: Improve this test?